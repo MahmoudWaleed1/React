c631d97e52d1fdc436834b78313fe80be093e8a7
Mahmoud Aboelsoaoud <mahmoudaboelsoaoud@gmail.com>
2025-09-09 22:36:27 +0300

    fixing login

--- src/ProtectedRoutes/ProtectedRoute.jsx
+++ src/ProtectedRoutes/ProtectedRoute.jsx
@@ -1,14 +1,9 @@
 import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom';
 import { authContext } from '../contexts/AuthContext';
-import LoginPage from "../pages/LoginPage"
 
 export default function ProtectedRoute({ children }) {
-
     const { isLoggedIn } = useContext(authContext)
 
-    return (
-        
-            isLoggedIn ? children : <LoginPage/>
-
-    )
+    return isLoggedIn ? children : <Navigate to="/login" />;
 }
